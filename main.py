15a450c6c4c7a98529888b393ed1383b9ba9ddef
Benjamin Tokgoez <23260775+benjamintokgoez@users.noreply.github.com>
2024-03-25 14:54:07 +0100

    add ENV vars to main.py

--- main.py
+++ main.py
@@ -5,26 +5,27 @@ import openai
 import pydoc
 import numpy
 import pandas
+import os
 
 
 #OpenAI vars
-openai.api_key = ""
-openai.api_type = ""
-openai.api_version = ""
-openai_deployment = None
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_type = os.environ.get("OPENAI_API_TYPE")
+openai.api_version = os.environ.get("OPENAI_API_VERSION")
+openai_deployment = os.environ.get("OPENAI_DEPLOYMENT")
 
 #AI Search vars
-aisearch_key = ""
-service_endpoint = ""
-index_name = ""
+aisearch_key = os.environ.get("AZURE_SEARCH_KEY")
+service_endpoint = os.environ.get("AZURE_SEARCH_ENDPOINT")
+index_name = os.environ.get("AZURE_SEARCH_INDEX_NAME")
 embedding_length = 1536
 
 #Azure SQL vars
-sql_server = ""
-database_name = ""
-username = ""
-password = ""
-sql_driver = ""
+sql_server = os.environ.get("SQL_SERVER_NAME")
+database_name = os.environ.get("SQL_DATABASE_NAME")
+username = os.environ.get("SQL_USERNAME")
+password = os.environ.get("SQL_PASSWORD")
+sql_driver = os.environ.get("SQL_DRIVER")
 
 
 
