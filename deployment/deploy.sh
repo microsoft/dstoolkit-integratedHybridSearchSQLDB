8ae2271b4f550f33413e6b14874d49345b190c7b
alschroe <heinz_metzing@web.de>
2024-03-28 21:58:45 +0100

    added IP

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 # Create all needed resources in azure
-export TF_VAR_region="us-west-2"
+export TF_VAR_region=""
 export TF_VAR_sqlpassword=""
+export TF_VAR_start_ip_address=$(hostname -I | awk '{print $1}')
+export TF_VAR_end_ip_address=$(hostname -I | awk '{print $1}')
 terraform init
 terraform plan -out "main.tfplan"
 terraform apply "main.tfplan"
-PREFIX = terraform state show random_string.random | grep "id"
-AZURE_SEARCH_KEY = $(az search admin-key show --resource-group aisearch-sql-integrated --service-name ${PREFIX}-aisearch --query primaryKey -o tsv)
-AZURE_SEARCH_ENDPOINT = "https://${PREFIX}-aisearch.search.windows.net"
-AZURE_SEARCH_INDEX_NAME = "aiindex"
-SQL_SERVER_NAME = "${PREFIX}-sqlserver-hybrid"
-SQL_DATABASE_NAME = "sqldb-hybrid"
-SQL_USERNAME = $(az sql server show --name ${PREFIX}-sqlserver-hybrid --resource-group aisearch-sql-integrated --query administratorLogin -o tsv)
-SQL_PASSWORD = ""
-SQL_DRIVER = ""
-OPENAI_API_KEY = $(az cognitiveservices account keys list --name ${PREFIX}-openaiaccount --resource-group aisearch-sql-integrated --query primaryKey -o tsv)
-OPENAI_API_TYPE = "azure"
-OPENAI_URI = "https://${PREFIX}-openaiaccount.openai.azure.com/"
-OPENAI_DEPLOYMENT = "text-embedding-ada-002"
+PREFIX=$(terraform state show random_string.random | grep "id")
+export AZURE_SEARCH_KEY=$(az search admin-key show --resource-group aisearch-sql-integrated --service-name ${PREFIX}-aisearch --query primaryKey -o tsv)
+export AZURE_SEARCH_ENDPOINT="https://${PREFIX}-aisearch.search.windows.net"
+export AZURE_SEARCH_INDEX_NAME="aiindex"
+export SQL_SERVER_NAME="${PREFIX}-sqlserver-hybrid"
+export SQL_DATABASE_NAME="sqldb-hybrid"
+export SQL_USERNAME=$(az sql server show --name ${PREFIX}-sqlserver-hybrid --resource-group aisearch-sql-integrated --query administratorLogin -o tsv)
+export SQL_PASSWORD=""
+export SQL_DRIVER=""
+export OPENAI_API_KEY=$(az cognitiveservices account keys list --name ${PREFIX}-openaiaccount --resource-group aisearch-sql-integrated --query primaryKey -o tsv)
+export OPENAI_API_TYPE="azure"
+export OPENAI_URI="https://${PREFIX}-openaiaccount.openai.azure.com/"
+export OPENAI_DEPLOYMENT="text-embedding-ada-002"
 cd ..
+
+# Run Pyhon script
+python3 main.py
 
 
 
